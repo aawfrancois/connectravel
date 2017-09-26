dcd55dc67a9e2eb069bf176321de3d2edddf9bc2
aawfrancois <aawfrancois@gmail.com>
2017-09-26 13:31:23 +0200

    reprendre

---
+++ public/js/BackEnd/userEdit.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $("#FormUser").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true
+            },
+            birth_date: {
+                required: true,
+                date: true
+            },
+            lastname: {
+                required: true
+            },
+            firstname: {
+                required: true
+            },
+            adress: {
+                required: true
+            },
+            postal_code: {
+                required: true,
+                digits: true
+            },
+            city: {
+                required: true
+            }
+        }
+    });
+});
